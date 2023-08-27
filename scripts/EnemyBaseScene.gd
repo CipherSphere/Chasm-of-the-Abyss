6f81dacb9671fb6177bb1c8c52ebda88098d8081
Ashton Martinez <martinezashton03@gmail.com>
2023-08-27 02:08:15 -0400

    Add enemy state management WIP, add enemy pursue logic

--- scripts/EnemyBaseScene.gd
+++ scripts/EnemyBaseScene.gd
@@ -6,18 +6,43 @@ extends CharacterBody2D
 
 @export var max_health: int
 @export var damage: int
+@export var move_speed: float
 
 
-const SPEED = 300.0
+
+
+var direction = Vector2(0, 0)
+
+var state = "idle"
+
+var enemy = null
+
+var player = null
 
 
 func _physics_process(delta):
+	print(state)
+	if state == "pursue":
+		
+		direction = (player.position - self.position).normalized()
 	
-	
-	var direction = Vector2(0, 0)
 	if direction:
-		velocity.x = direction * SPEED
+		velocity = direction * move_speed
+		
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+		velocity.y = move_toward(velocity.y, 0, move_speed)
 
 	move_and_slide()
+
+
+func _on_sense_area_area_entered(area):
+	if area.get_parent().type == "player":
+		state = "pursue"
+		player = area.get_parent()
+
+
+func _on_sense_area_area_exited(area):
+	if area.get_parent().type == "player":
+		state = "idle"
+		direction = Vector2(0, 0)
