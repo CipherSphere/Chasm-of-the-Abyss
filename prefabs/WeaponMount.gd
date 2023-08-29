0f6ecee6d484832b6dfae2178ee5194acde620dc
Ashton Martinez <martinezashton03@gmail.com>
2023-08-29 19:56:43 -0400

    Add timer

--- prefabs/WeaponMount.gd
+++ prefabs/WeaponMount.gd
@@ -3,11 +3,13 @@ extends Node2D
 var parent
 var weapon
 
+var can_attack = true
+
 func _ready():
 	parent = $"../.."
 	if parent.weapon != null:
 		weapon = parent.weapon
-	
+		
 
 func _input(event):
 	if event.is_action_pressed("light_attack"):
@@ -16,13 +18,21 @@ func _input(event):
 	
 
 func attack():
-	var new_attack = weapon.instantiate()
-	new_attack.position = $"..".global_position
-	new_attack.rotation = self.global_rotation
+	if can_attack:
+		
+		var new_attack = weapon.instantiate()
+		$"../AttackCooldown".wait_time = new_attack.cool_down
+		can_attack = false
+		$"../AttackCooldown".start()
+		new_attack.position = $"..".global_position
+		new_attack.rotation = self.global_rotation
+		
+		self.get_parent().get_parent().get_parent().add_child(new_attack)
+
+
+
+func _on_attack_cooldown_timeout():
+	print("cooldown off")
 	
-	self.get_parent().get_parent().get_parent().add_child(new_attack)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	can_attack = true
+	$"../AttackCooldown".stop()
